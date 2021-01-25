b8edfcdd7af7f29ceced7d43f22e567031868a41
Patrick Tsai <patosai@users.noreply.github.com>
2021-01-24 19:12:16 -0800

    Work on cartpole v1

--- README.md
+++ README.md
@@ -3,9 +3,21 @@ OpenAI Gym
 
 Here I attempt to tackle the puzzles in OpenAI gym using a variety of machine learning techniques.
 
+## Terms
+- Observation: what the agent, in this case machine learning models, sees from the environment
+- Action: something the agent does to the environment
+- Agent: something that takes observations and outputs actions. I also call this a 'model'
+- Episode: a series of actions/observations
+
+Reinforcement learning is based around seeing how an agent performs in an episode, seeing the reward it gets, and updating the agent so the it receives more reward.
+
 ### Simple Reinforcement Learning
 
-The basic concept is, for each episode, I keep track of the observations and resulting sampled actions. Depending if the reward is greater than the average of previous episodes, the actions are encouraged or discouraged by updating gradients on a fully connected neural network. This update is done after every episode.
+The basic concept is that during an episode, I sample actions and save the observation/action pairs. After the episode is over, I check the received reward. If the reward is greater than the rewards seen in previous episodes, I update the gradients on the model so that every action taken in the episode is slightly more likely to be taken by the model during the next episode, given the same observation. If the reward is less, I update the gradients so that the opposite happens.
 
-The key idea is that in the short term, while some actions may not be optimal but they're buoyed by better actions at other times, in the long run these non-optimal actions will be averaged out and corrected by increasingly better actions.
+In technical terms, I update model gradients using backpropagation with a multiplier associated with the difference in reward.
 
+Even though in the beginning this update strategy may encourage suboptimal or damaging actions, in the long run the accumulation of actions will average out and the worse actions will be disincentivized.
+
+Current results on Cartpole v1:
+![](images/cartpole_v1.png)
--- simple_reinforcement_learning.py
+++ simple_reinforcement_learning.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3
 
 import gym
+import matplotlib.pyplot as plt
+import numpy as np
 import torch
+
+
+gym_name = 'CartPole-v1'
+model_filename = "cartpole_v1.pt"
+gym_max_steps = 200 # max steps for cartpole is 200
 
 
 def create_env():
     # print(gym.envs.registry.all())
-    env = gym.make('CartPole-v0')
+    env = gym.make(gym_name)
     print("observation space: ", env.observation_space)
     print("observation space shape: ", env.observation_space.shape)
     print("observation space high: ", env.observation_space.high)
@@ -25,11 +32,21 @@ def create_model(env):
     assert output_size is not None, "implement something to calculate the output_size"
 
     return torch.nn.Sequential(
-        torch.nn.Linear(input_size, 4),
-        torch.nn.Linear(4, 4),
-        torch.nn.Linear(4, output_size),
-        torch.nn.Softmax(),
+        torch.nn.Linear(input_size, input_size*output_size),
+        torch.nn.Linear(input_size*output_size, input_size*output_size),
+        torch.nn.Linear(input_size*output_size, input_size*output_size),
+        torch.nn.Linear(input_size*output_size, output_size),
+        torch.nn.Softmax(dim=0),
     )
+
+
+def save_model(model, filename=model_filename):
+    torch.save(model.state_dict(), filename)
+
+
+def load_model(model, filename=model_filename):
+    model.load_state_dict(torch.load(filename))
+    model.eval()
 
 
 def observation_to_model_action_and_gradient(env, model, observation):
@@ -39,28 +56,57 @@ def observation_to_model_action_and_gradient(env, model, observation):
 
     if isinstance(env.action_space, gym.spaces.Discrete):
         action = torch.multinomial(model_output, 1, replacement=True)
-        action_one_hot = torch.nn.functional.one_hot(action, model_output.numel())[0]
-        gradient = torch.dot(model_output, action_one_hot.float())
+        num_model_outputs = model_output.numel()
+        one_hot = torch.zeros(num_model_outputs)
+        one_hot[action] = 1
+        # print("action", action,
+        #       "model output", model_output,
+        #       "weights", weights)
+        gradient = torch.dot(model_output, one_hot.float())
 
     assert action is not None, "implement something to calculate model action"
 
     return action, gradient
 
 
-def main():
+def plot_reward(reward_per_episode, title="", show=True):
+    plt.plot(range(len(reward_per_episode)), reward_per_episode)
+    plt.xlabel('Episode #')
+    plt.ylabel('Reward')
+    plt.title(title)
+    if show:
+        plt.show()
+
+
+def constrain(x, minimum, maximum):
+    if x > maximum:
+        return maximum
+    elif x < minimum:
+        return minimum
+    else:
+        return x
+
+
+def moving_average(x, w):
+    return np.convolve(x, np.ones(w), 'valid') / w
+
+
+def train():
+    print("Training")
+
+    episode_rewards = []
+
     with create_env() as env:
         model = create_model(env)
 
-        episode_rewards = []
-
-        for episode_num in range(20):
-            print("episode {}".format(episode_num))
-
+        for episode_num in range(1500):
             observation_gradient_pairs = []
             observation = env.reset()
 
             total_reward = 0
-            for t in range(100):
+
+            num_steps_taken = gym_max_steps
+            for t in range(gym_max_steps):
                 env.render()
 
                 observation = torch.Tensor(observation)
@@ -71,23 +117,68 @@ def main():
                 total_reward += reward
 
                 if done:
-                    print("episode finished after {} timesteps, {} reward".format(t+1, total_reward))
+                    num_steps_taken = t+1
                     break
 
+            print("episode {}, {} timesteps, {} reward".format(episode_num, num_steps_taken, total_reward))
+            if num_steps_taken == gym_max_steps:
+                save_model(model, model_filename)
+
             # update the model
-            episode_rewards.append(total_reward)
             if len(episode_rewards) > 0:
-                previous_average_reward = sum(episode_rewards)/len(episode_rewards)
-                difference = previous_average_reward - total_reward
+                last_few_episodes = episode_rewards[-5:]
+                previous_average_reward = sum(last_few_episodes)/len(last_few_episodes)
+                difference = total_reward - previous_average_reward
+
                 model.zero_grad()
+
                 for observation, gradient in observation_gradient_pairs:
                     # compute gradients; stacked calls to .backward() without a zero_grad will sum the gradients
                     gradient.backward()
                 with torch.no_grad():
+                    learning_rate = 0.001
+                    max_learning_rate = 0.01
+                    reward_multiplier = learning_rate * difference
+
+                    effective_learning_rate = constrain(reward_multiplier, -1*max_learning_rate, max_learning_rate)
                     for param in model.parameters():
-                        learning_rate = 0.005
-                        param -= difference * learning_rate * param.grad
+                        param += effective_learning_rate * param.grad
+            episode_rewards.append(total_reward)
+
+    plt.subplot(2, 1, 1)
+    plot_reward(episode_rewards, "No averaging", show=False)
+    plt.subplot(2, 1, 2)
+    plot_reward(moving_average(episode_rewards, 10), "10 episode moving average", show=True)
+
+
+def replay():
+    print("Replaying")
+    with create_env() as env:
+        model = create_model(env)
+        load_model(model, model_filename)
+
+        episode_rewards = []
+
+        for episode_num in range(100):
+            num_steps_taken = 0
+            observation = env.reset()
+            total_reward = 0
+            for t in range(gym_max_steps):
+                env.render()
+
+                num_steps_taken += 1
+                observation = torch.Tensor(observation)
+                action, gradient = observation_to_model_action_and_gradient(env, model, observation)
+                observation, reward, done, info = env.step(action.item())
+
+                total_reward += reward
+
+                if done:
+                    break
+            episode_rewards.append(total_reward)
+            print("episode {}, {} timesteps, {} reward".format(episode_num, num_steps_taken, total_reward))
+    plot_reward(episode_rewards)
 
 
 if __name__ == "__main__":
-    main()
+    train()
